bd61b504d5bfd5c746ce769892b3ef99f0fbfec4
Safouane Jelassi <safouane.jelassi@precicom.com>
2022-10-12 13:51:46 -0400

    init project + init ngrx + install and implement ngrx devtools

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { RouterModule } from '@angular/router';
+import { RegisterComponent } from 'src/app/auth/components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { reducers } from 'src/app/auth/store/reducers';
+import { AuthService } from 'src/app/auth/services/auth.service';
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers),
+  ],
+  declarations: [RegisterComponent],
+  providers: [AuthService],
+})
+export class AuthModule {}
---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { registerAction } from 'src/app/auth/store/actions';
+import { isSubmittingSelector } from 'src/app/auth/store/selectors';
+
+@Component({
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  // $ for streams
+  isSubmitting$: Observable<boolean>;
+  constructor(private fb: FormBuilder, private store: Store) {}
+  ngOnInit(): void {
+    this.initializeForm();
+    this.initializeValues();
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+  }
+
+  onSubmit() {
+    this.store.dispatch(registerAction(this.form.value));
+  }
+}
